a3dd2987f4b5cbf5ec6155ce595c0d0b3bae992f
pabloromero0428 <pablo.romero2840@gmail.com>
2020-04-01 18:11:57 -0500

    latitud y logintud + market funcional login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
@@ -16,6 +17,8 @@ import { BoardUserComponent } from './components/board-user/board-user.component
 
 import { authInterceptorProviders } from './helpers/auth.interceptor';
 
+import { MapviewComponent } from './components/mapview/mapview.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,13 +28,17 @@ import { authInterceptorProviders } from './helpers/auth.interceptor';
     ProfileComponent,
     BoardAdminComponent,
     BoardModeratorComponent,
-    BoardUserComponent
+    BoardUserComponent,
+    MapviewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCWOR0jWaJbXeWJ6-UgMOIPT_d7O1idxiE'
+    })
   ],
   providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
---
+++ src/app/components/mapview/mapview.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { Marcador } from '../../class/marcador.class';
+
+
+
+
+
+
+@Component({
+  selector: 'app-mapview',
+  templateUrl: './mapview.component.html',
+  styleUrls: ['./mapview.component.css']
+})
+
+export class MapviewComponent implements OnInit {
+
+  coordsActua: any;
+  marcadores: Marcador[] = [];
+
+  lat: number;
+  lng: 75.61827329202217;
+
+  mostrarUbicacion(ubicacion) {
+    console.log('entre aqui')
+    const lng = ubicacion.coords.longitude;
+    const lat = ubicacion.coords.latitude;
+    console.log(`longitud: ${lng} | latitud: ${lat}`);
+
+    // console.log('longitud: ${lng} | latitud: ${lat}');
+    // // const nuevoMarcador = new Marcador(lat, lng);
+    // // this.marcadores.push(nuevoMarcador);
+    // const nuevoMarcador = new Marcador(6.150344, -75.61827329202217);
+    // this.marcadores.push(nuevoMarcador);
+    // console.log(this.marcadores);
+
+  }
+
+  constructor() {
+
+    // this.marcadores = [];
+
+
+    const onUbicacionConcedida = ubicacion => {
+      this.marcadores = [];
+      const nuevoMarcador = new Marcador(ubicacion.coords.latitude, ubicacion.coords.longitude);
+      this.marcadores.push(nuevoMarcador);
+      console.log(this.marcadores);
+    }
+
+    const onErrorDeUbicacion = err => {
+      console.log("Error obteniendo ubicación: ", err);
+    }
+
+    const opcionesDeSolicitud = {
+      enableHighAccuracy: true, // Alta precisión
+      maximumAge: 0, // No queremos caché
+      timeout: 5000 // Esperar solo 5 segundos
+    };
+    // Solicitar
+    navigator.geolocation.getCurrentPosition(onUbicacionConcedida, onErrorDeUbicacion, opcionesDeSolicitud);
+
+
+  }
+
+  ngOnInit(): void {
+
+
+  }
+
+  abc(ubicacion) {
+
+  }
+
+  cambiarMarcador(evento) {
+
+    this.marcadores = [];
+    const coords: { lat: number, lng: number } = evento.coords;
+    const nuevoMarcador = new Marcador(coords.lat, coords.lng);
+    this.marcadores.push(nuevoMarcador);
+    console.log(this.marcadores);
+  }
+}
